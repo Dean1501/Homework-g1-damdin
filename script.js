391bbd6c5a3484288fa0e178e767a62c7b77c8b0
Dean1501 <damdinbatorov1992@gmail.com>
2022-12-28 16:43:59 +0300

    hw13 done

---
+++ script.js
@@ -0,0 +1,16 @@
+let listUsers = {
+    admin: `root`,
+    user: `123`,
+    Damdin: `150192`,
+};
+
+let login = prompt(`Логин: `);
+let password = prompt(`Пароль: `);
+
+    if (listUsers[login] !== undefined && listUsers[login] == password) {
+        alert(`Вы вошли!`)
+    } else if (listUsers[login] == undefined) {
+        alert(`Такого пользователя не существует`);
+    } else if (password != listUsers[login] || password == null || password == undefined) {
+        alert(`Неправильный пароль`);
+    };
