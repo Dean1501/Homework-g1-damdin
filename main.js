182748a0a88d7c25d22b70c0326313b92d092f53
Dean1501 <damdinbatorov1992@gmail.com>
2023-01-05 10:43:22 +0300

    change listUsers

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 import users from "/listUsers.js";
-alert(JSON.stringify(users));
+
 
 let login = prompt(`Логин: `);
 let password = prompt(`Пароль: `);
 
-if (listUsers[login] !== undefined && listUsers[login] == password) {
+if (users[login] !== undefined && users[login] == password) {
         alert(`Вы вошли!`)
-    } else if (listUsers[login] == undefined) {
+    } else if (users[login] == undefined) {
         alert(`Такого пользователя не существует`);
-    } else if (password != listUsers[login] || password == null || password == undefined) {
+    } else if (password != users[login] || password == null || password == undefined) {
         alert(`Неправильный пароль`);
     };
     
