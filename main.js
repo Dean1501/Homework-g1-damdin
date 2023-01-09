6e28587cb38f73fef5a5ef0b8244f91c98a80d5a
Dean1501 <damdinbatorov1992@gmail.com>
2023-01-09 17:42:41 +0300

    menu items out

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 import ingredientsPrice from "./ingredientsPrice.json" assert {type: 'json'};
 
-let menu = [
-    frenchFries = {
-        name: "frenchFries",
-        ingredients: ["potato", "salt",],
-    },
-    sandwich = {
-        name: "sandwich",
-        ingredients: ["bread", "sausage", "mayo",],
+let frenchFries = {
+    name: "frenchFries",
+    ingredients: ["potato", "salt",],
+};
 
-    },
-    buuza = {
-        name: "buuza",
-        ingredients: ["mutton", "beef", "pork", "onion", "salt", "pepper", "flour", "water", "salt",],
-    },
-];
+let sandwich = {
+    name: "sandwich",
+    ingredients: ["bread", "sausage", "mayo",],
+};
+
+let buuza = {
+    name: "buuza",
+    ingredients: ["mutton", "beef", "pork", "onion", "salt", "pepper", "flour", "water", "salt",],
+};
+
+let menu = [frenchFries, sandwich, buuza];
 
 let sebesMenu = menu.index.ingredients.reduce(function (sum) {
     console.log(sebesMenu)
