2cb5b4b24dc51e73a804397cd7dfbd5d47f71e79
Dean1501 <damdinbatorov1992@gmail.com>
2023-01-09 12:12:39 +0300

    change js to json

--- listUsers.js
+++
@@ -1,7 +0,0 @@
-let listUsers = {
-    admin: `root`,
-    user: `123`,
-    Damdin: `150192`,
-};
-
-export default listUsers;
--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-import users from "/listUsers.js";
-
+import users from "/listUsers.json";
 
 let login = prompt(`Логин: `);
 let password = prompt(`Пароль: `);
 
 if (users[login] !== undefined && users[login] == password) {
-        alert(`Вы вошли!`)
-    } else if (users[login] == undefined) {
-        alert(`Такого пользователя не существует`);
-    } else if (password != users[login] || password == null || password == undefined) {
-        alert(`Неправильный пароль`);
-    };
-    
+    alert(`Вы вошли!`)
+} else if (users[login] == undefined) {
+    alert(`Такого пользователя не существует`);
+} else if (password != users[login] || password == null || password == undefined) {
+    alert(`Неправильный пароль`);
+};
