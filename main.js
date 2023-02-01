88df0d56ec4b8f3006bfa205b04757facf05991d
Dean1501 <damdinbatorov1992@gmail.com>
2023-02-01 10:32:20 +0300

    add 2 & 3

--- main.js
+++ main.js
@@ -2,19 +2,30 @@ import menu from "./menu.json" assert {type: 'json'};
 import ingredientsPrice from "./ingredientsPrice.json" assert {type: 'json'};
 
 // 1.
-menu.forEach(function(element) {
-    let costPrice = element.ingredients.reduce(function (sum, item){
+menu.forEach(function (element) {
+    let costPrice = element.ingredients.reduce(function (sum, item) {
         return sum + ingredientsPrice[item]
     }, 0)
     console.log(costPrice)
 });
 
+// 2. 
+
 let costSum = menu.map(function (menuItem) {
     let costPrice = menuItem.ingredients.reduce(function (sum, item) {
         return sum + ingredientsPrice[item]
-    }, 0)
-    menuItem.cost = costPrice;
-    return menuItem;
+    }, 0);
+    let newMenuItem = Object.assign({}, menuItem);
+    newMenuItem.cost = costPrice;
+    return newMenuItem;
 });
-console.log(costSum)
+console.log(costSum);
 
+// 3. 
+
+let vegetFood = menu.some(function (menuElem) {
+    return menuElem.ingredients.every(function (ingredient) {
+        return ingredient !== "meat" & ingredient !== "mutton" & ingredient !== "beef" & ingredient !== "pork" & ingredient !== "sausage";
+    });
+});
+console.log(vegetFood);
