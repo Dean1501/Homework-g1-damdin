3fcaebee75ea01710dfa492e5d1b6257e0ddc38f
Dean1501 <damdinbatorov1992@gmail.com>
2023-02-09 11:58:52 +0300

    first commit 15 lesson

---
+++ main.js
@@ -0,0 +1,12 @@
+setTimeout(() => {
+    console.log("start");
+}, 7000);
+
+let menu = fetch("*/menu.json")
+    .then((response) => response.json)
+    .then((data) => console.log(data));
+    setTimeout(() => {
+        console.log("start menu")
+    }, 1000);
+
+console.log(menu);
