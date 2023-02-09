814778f9772d9fc74b396c66f07b8c70c14987d5
Dean1501 <damdinbatorov1992@gmail.com>
2023-02-09 17:35:21 +0300

    one more try

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
-setTimeout(() => {
-    console.log("start");
-}, 7000);
-
-fetch("menu.json", {
-    mode: "cors",
-    headers: {
-        "Content-Type": "application/json",
-    },
-})
-    .then((response) => response.json)
-    .then((data) => data + 123);
-    setTimeout(() => {
-        console.log("start menu")
-    }, 1000);
-console.log(menu);
+let menu = fetch("menu.json")
+    .then((response) => response.json())
+    .then((data) => {
+        menu = data;
+        console.log(menu)
+    })
+    .then((price) => {
+        return fetch(ingredientsPrice.json)
+            .then((response) => response.json())
+            .then((price) => {
+                ingredientsPrice = price;
+                console.log(ingredientsPrice);
+            });
+    })
+    .then((costPrice) => {
+        menu.forEach((element) => {
+            let costPrice = element.ingredients.reduce((sum, item) => {
+                return sum + ingredientsPrice[item]
+            }, 0)
+            console.log(costPrice);
+        });
+    })
+    .then((costSum) => {
+        let costSum = menu.map((menuItem) => {
+            let costPrice = menuItem.ingredients.reduce((sum, item) => {
+                return sum + ingredientsPrice[item]
+            }, 0);
+            console.log(costSum)
+        });
+    }) ;
